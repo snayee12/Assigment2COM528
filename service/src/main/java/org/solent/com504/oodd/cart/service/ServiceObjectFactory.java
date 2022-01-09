7e3dac35eaa6756352215ded9131026735e799ef
snayee12 <46923057+snayee12@users.noreply.github.com>
2022-01-09 01:07:28 +0000

    Add files via upload

---
+++ service/src/main/java/org/solent/com504/oodd/cart/service/ServiceObjectFactory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.solent.com504.oodd.cart.service;
+
+import org.solent.com504.oodd.cart.model.service.ShoppingCart;
+import org.solent.com504.oodd.cart.model.service.ShoppingService;
+//import org.solent.com504.oodd.cart.model.dto.
+
+/**
+ *
+ * @author cgallen
+ */
+public class ServiceObjectFactory {
+    
+    static ShoppingService shoppingService = new ShoppingServiceImpl();
+    
+    // cannot instantiate
+    private ServiceObjectFactory(){
+        
+    }
+    
+    public static ShoppingService getShoppingService(){
+        return shoppingService;
+    }
+    
+    public static ShoppingCart getNewShoppingCart(){
+        return new ShoppingCartImpl();
+    }
+//    public static BankAccount getNewBankAccount() {
+//        return new BankAccount();
+//    }
+    
+}
